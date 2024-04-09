8a977bf5796abfa8e33317628d4aec21f9dd3fa2
Naimur Rahman <naimur2231@gmail.com>
2024-04-09 02:36:43 -0700

    initialization project setup

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+// import router from "./app/routes";
+
+const app: Application = express();
+
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Server is working....!",
+  });
+});
+
+// app.use("/api", router);
+
+export default app;
