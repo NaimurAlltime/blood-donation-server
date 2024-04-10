a1c129a6d594db3376245f0ee5c048ebc1af8d8c
Naimur Rahman <naimur2231@gmail.com>
2024-04-10 07:16:24 -0700

    implement Authorization in auth middleware

--- src/app/modules/Request/request.routes.ts
+++ src/app/modules/Request/request.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { RequestController } from "./request.controller";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
-router.post("/donation-request", RequestController.requestDonation);
+router.post("/donation-request", auth(), RequestController.requestDonation);
 
 export const RequestRoutes = router;
