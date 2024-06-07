f83fd01018a69d58a43488741178c100d382b70e
Naimur Rahman <naimur2231@gmail.com>
2024-06-08 02:30:30 +0600

    project done

--- src/app.ts
+++ src/app.ts
@@ -5,24 +5,16 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
-const allowedOrigins = ["https://blood-donation-client-theta.vercel.app"];
+app.use(
+  cors({
+    origin: ["https://blood-donation-client-theta.vercel.app"],
+    credentials: true,
+  })
+);
 
-const corsOptions: cors.CorsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin as string)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ origin: "*" }));
 
 app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({
